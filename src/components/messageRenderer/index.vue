a9619bd2b4d101b4449b75c291e8876c384d4aa3
Ayoub Rezaei <ayoubrezaei79@gmail.com>
2025-11-27 13:54:56 +0100

    Add messageRenderer component for dynamic message rendering based on type

---
+++ src/components/messageRenderer/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <component :is="componentToRender" :message="message" />
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import type { Message } from "../../types";
+import TextBubble from "../textBubble/index.vue";
+import RewardCard from "../rewardCard/index.vue";
+
+interface Props {
+  message: Message;
+}
+
+const componentMap: Record<string, any> = {
+  A: TextBubble,
+  B: RewardCard,
+};
+
+const componentToRender = computed(() => {
+  return componentMap[props.message.type] || TextBubble;
+});
+
+const props = defineProps<Props>();
+</script>
